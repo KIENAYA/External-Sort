19eaa077429833df5c0dc63f73ceb9e8ad764787
Just Neik <100520908+KIENAYA@users.noreply.github.com>
2025-07-14 10:35:01 +0700

    Update main.go
    update

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -18,28 +18,28 @@ func main() {
 
 	os.MkdirAll(cfg.TmpDir, os.ModePerm)
 
-	fmt.Println("🔄 Sorting chunks...")
+	fmt.Println("Sorting chunks...")
 	start := time.Now()
 	chunks, err := sorter.SortChunksParallel(cfg.InputFile, cfg.TmpDir, cfg.ChunkSize)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("✅ Sorted %d chunks in %v\n", len(chunks), time.Since(start))
+	fmt.Printf("Sorted %d chunks in %v\n", len(chunks), time.Since(start))
 
-	fmt.Println("🔄 Merging chunks...")
+	fmt.Println("Merging chunks...")
 	start = time.Now()
 	err = sorter.MergeChunksWithBatch(chunks, cfg.OutputFile, cfg.MergeBatchSize)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("✅ Merged into %s in %v\n", cfg.OutputFile, time.Since(start))
+	fmt.Printf("Merged into %s in %v\n", cfg.OutputFile, time.Since(start))
 
-	fmt.Println("🔍 Validating output file...")
+	fmt.Println("Validating output file...")
 	start = time.Now()
 	if err := sorter.ValidateSortedFile(cfg.OutputFile); err != nil {
-		log.Fatalf("❌ Validation failed: %v", err)
+		log.Fatalf("Validation failed: %v", err)
 	} else {
-		fmt.Printf("✅ Validation passed in %v\n", time.Since(start))
+		fmt.Printf("Validation passed in %v\n", time.Since(start))
 	}
 
 }
