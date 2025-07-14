4306a56f83eb0306aa9f77b58134a0a7d66e787b
Just Neik <100520908+KIENAYA@users.noreply.github.com>
2025-07-14 10:33:48 +0700

    Update generate-data.go
    Update

--- generate-data/generate-data.go
+++ generate-data/generate-data.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-	const total = 1_000
-	const output = "input1.txt"
+	const total = 1_000_000_000
+	const output = "input.txt"
 
 	f, err := os.Create(output)
 	if err != nil {
@@ -18,19 +18,19 @@ func main() {
 	}
 	defer f.Close()
 
-	writer := bufio.NewWriterSize(f, 4*1024*1024) // 4MB buffer
+	writer := bufio.NewWriterSize(f, 4*1024*1024) 
 	defer writer.Flush()
 
 	rand.Seed(time.Now().UnixNano())
 
 	start := time.Now()
 	for i := 0; i < total; i++ {
-		n := rand.Intn(2_000) // ví dụ giới hạn trong 2 tỷ
+		n := rand.Intn(2_000_000_000) 
 		fmt.Fprintln(writer, n)
 
 		// if i > 0 && i%10_000_000 == 0 {
-		// 	fmt.Printf("✅ Đã ghi %d dòng...\n", i)
+		// 	fmt.Printf(" Đã ghi %d dòng...\n", i)
 		// }
 	}
-	fmt.Println("🎉 Hoàn tất! Thời gian:", time.Since(start))
+	fmt.Println("Completed in:", time.Since(start))
 }
